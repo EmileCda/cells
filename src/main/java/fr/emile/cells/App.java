0b12a08965ac7ab4b13c4397fefcbcd674669b3f
emileCda <emile.cda@gmail.com>
2023-04-23 22:52:05 +0200

    Evironment project cells : ready

---
+++ src/main/java/fr/emile/cells/App.java
@@ -0,0 +1,13 @@
+package fr.emile.cells;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
